813cdeba6f9ffe3c24b0e17ff6fe947e15911f5e
Fedor <feodor.a.v.03@gmail.com>
2025-04-16 22:17:24 +0300

    fix config

--- bot/core/config.py
+++ bot/core/config.py
@@ -17,10 +17,10 @@ class EnvBaseSettings(BaseSettings):
 
 class WebhookSettings(EnvBaseSettings):
     USE_WEBHOOK: bool
-    WEBHOOK_BASE_URL: str = "https://api.top-kod.ru"
-    WEBHOOK_PATH: str = "/webhook"
-    WEBHOOK_SECRET: str = ""
-    WEBHOOK_HOST: str = "localhost"
+    WEBHOOK_BASE_URL: str
+    WEBHOOK_PATH: str 
+    WEBHOOK_SECRET: str 
+    WEBHOOK_HOST: str
     WEBHOOK_PORT: int = 8080
 
     @property
