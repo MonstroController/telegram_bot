bf93196fcac446b6b34a2425c0de8631d8e21e3b
Fedor <feodor.a.v.03@gmail.com>
2025-07-23 20:10:43 +0300

    test2

--- bot/middlewares/auth.py
+++ bot/middlewares/auth.py
@@ -29,20 +29,20 @@ class AuthMiddleware(BaseMiddleware):
             return await handler(event, data)
     
         session: AsyncSession = data["session"]
-        message: Message = event
+        message: Message = event.message
         logger.info(f"Message: {message}")
-        logger.info(f"2 auth middleware: {message.from_user}")
-        # if not user:
-        #     return await handler(event, data)
+        user = message.from_user
+        if not user:
+            return await handler(event, data)
 
-        # logger.info(f"23 auth middleware: {message.from_user}")
-        # if await user_exists(session, user.id):
-        #     return await handler(event, data)
+        logger.info(f"23 auth middleware: {message.from_user}")
+        if await user_exists(session, user.id):
+            return await handler(event, data)
 
-        # referrer = find_command_argument(message.text)
+        referrer = find_command_argument(message.text)
 
-        # logger.info(f"new user registration | user_id: {user.id} | message: {message.text}")
+        logger.info(f"new user registration | user_id: {user.id} | message: {message.text}")
 
-        # await add_user(session=session, user=user, referrer=referrer)
+        await add_user(session=session, user=user, referrer=referrer)
 
         return await handler(event, data)
